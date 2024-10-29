9f22f0e11e45ddad0299974dc42d44730bc59dab
Yaroslav <aroslavmikirticev@gmail.com>
2024-10-29 23:36:25 +0200

    second commit

--- project_4.1.cpp
+++ project_4.1.cpp
@@ -6,26 +6,19 @@
 using namespace std;
 
 int main()
-{ 
-    /*SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    int a, b;
-    cout << "Enter a, b:" << endl;
-    cin >> a >> b;
-    ((a % 2 != 0) && (b % 2 == 0)) || ((b % 2 != 0) && (a % 2 == 0)) ? cout  << "Непарне" : cout << "Парне";*/
-    
+{
+
+
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     int a, b;
-    cout << "\nВведіть a: ";
+    cout << "\nEnter a: ";
     cin >> a;
-    cout << "\nВведіть b: ";
+    cout << "\nEnter b: ";
     cin >> b;
-    cout << "\n Одне з чисел непарне  " << boolalpha << (a%2 != 0 || b%2 !=0) << endl;
+    cout << "\n One of the numbers is odd" << boolalpha << (a % 2 != 0 || b % 2 != 0) << endl;
 
-    
-   
-    /*Задано два цілих числа: A, B. Перевірити істинність висловлення:
-    «Тільки одне з чисел A і B непарне». */
+    return 0;
+
 }
 
