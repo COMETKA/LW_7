caa9e26af77b32982e4272bbc98f26e7b53ec719
Yaroslav <aroslavmikirticev@gmail.com>
2024-12-11 03:30:13 +0200

    change number 3

--- project_4.1.cpp
+++ project_4.1.cpp
@@ -11,11 +11,13 @@ int main()
 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int a, b;
+    int a, b, c;
     cout << "\nEnter a: ";
     cin >> a;
     cout << "\nEnter b: ";
     cin >> b;
+    cout << "\nEnter c: ";
+    cin >> c;
     cout << "\n One of the numbers is odd" << boolalpha << (a % 2 != 0 || b % 2 != 0) << endl;
 
     return 0;
